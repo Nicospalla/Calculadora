ba73e4e8bf11a16d56b5d0ac7f614cd166e171ca
Nicolas Spalla <nicolasaspalla@gmail.com>
2024-01-05 15:35:22 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Calculadora
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(48, 36);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(523, 70);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "NUMERO";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(48, 374);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(72, 65);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(148, 374);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(72, 65);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumar.Location = new System.Drawing.Point(48, 457);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(72, 65);
+            this.btnSumar.TabIndex = 3;
+            this.btnSumar.Text = "+";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.Location = new System.Drawing.Point(499, 447);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(72, 65);
+            this.btnIgual.TabIndex = 4;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 534);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "frmPrincipal";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnIgual;
+    }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmPrincipal : Form
+    {
+
+        float numeroA;
+        float numeroB;
+        int bandera;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            lblResultado.Text = lblResultado.Text == "NUMERO" ? "0" : lblResultado.Text;
+        }
+        private void refreshPantalla()
+        {
+            var renovado = lblResultado.Text;
+            if (lblResultado.Text.StartsWith("0"))
+            {
+                char[] numeros = renovado.ToCharArray();
+                renovado = new string(numeros,1,numeros.Length -1);
+
+            }
+            lblResultado.Text = renovado;
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text += 1;
+            refreshPantalla();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text += 2;
+            refreshPantalla();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            bandera = 1;
+            numeroA =int.Parse(lblResultado.Text);
+            numeroB = 0;
+            lblResultado.Text = numeroB.ToString();
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            numeroB = int.Parse(lblResultado.Text);
+            switch (bandera)
+            {
+                case 1:
+                    lblResultado.Text = (numeroA + numeroB).ToString();
+                    break;
+
+            }
+        }
+    }
+}
